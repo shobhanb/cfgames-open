22cbfe8683e3b6984e2e4b3a06a00467d7d434fe
Shobhan Bhatt <shobhanb@gmail.com>
2025-06-17 11:28:43 +0530

    Added theme switcher

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/toast/toast.component';
 import { ModalComponent } from './shared/modal/modal.component';
 import { UserAuthService } from './shared/user-auth/user-auth.service';
+import { ThemeService } from './shared/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { UserAuthService } from './shared/user-auth/user-auth.service';
 })
 export class AppComponent implements OnInit {
   private userAuth = inject(UserAuthService);
+  private themeService = inject(ThemeService);
 
   ngOnInit(): void {
     this.userAuth.loginWithLocalToken();
--- frontend/src/app/shared/pages/nav-menu/nav-menu.component.ts
+++ frontend/src/app/shared/pages/nav-menu/nav-menu.component.ts
@@ -1,24 +1,26 @@
 import { Component, computed, inject } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { heroBars3 } from '@ng-icons/heroicons/outline';
+import { heroBars3, heroMoon, heroSun } from '@ng-icons/heroicons/outline';
 import { TitleService } from '../../title.service';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { UserAuthService } from '../../user-auth/user-auth.service';
 import { ToastService } from '../../toast/toast.service';
 import { LoggedInButtonComponent } from './logged-in-button/logged-in-button.component';
 import { EventService } from '../../event.service';
+import { ThemeService } from '../../theme.service';
 
 @Component({
   selector: 'app-nav-menu',
   imports: [NgIcon, RouterLink, RouterLinkActive, LoggedInButtonComponent],
-  viewProviders: [provideIcons({ heroBars3 })],
+  viewProviders: [provideIcons({ heroBars3, heroSun, heroMoon })],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css',
 })
 export class NavMenuComponent {
   titleService = inject(TitleService);
   userAuth = inject(UserAuthService);
-  toastService = inject(ToastService);
+  private toastService = inject(ToastService);
+  themeService = inject(ThemeService);
   private eventService = inject(EventService);
 
   constructor() {
