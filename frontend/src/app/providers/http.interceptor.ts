d1006426928fafbc807faae94aa199c62c140017
Shobhan Bhatt <shobhanb@gmail.com>
2025-07-04 22:47:14 +0530

    Changed interceptor to observables

--- frontend/src/app/providers/http.interceptor.ts
+++ frontend/src/app/providers/http.interceptor.ts
@@ -1,19 +1,24 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { Auth } from '@angular/fire/auth';
+import { from, switchMap } from 'rxjs';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject(AuthService);
-  const token = authService.token();
-
-  if (token) {
-    const authRequest = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return next(authRequest);
-  }
-
-  return next(req);
+  const auth = inject(Auth);
+  // getIdToken() returns a Promise<string|null>
+  return from(
+    auth.currentUser ? auth.currentUser.getIdToken() : Promise.resolve(null)
+  ).pipe(
+    switchMap((token) => {
+      if (token) {
+        const authRequest = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        return next(authRequest);
+      }
+      return next(req);
+    })
+  );
 };
