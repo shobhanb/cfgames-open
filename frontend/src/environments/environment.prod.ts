7d5cda50c12c4a918234dfea81fe5f039153ee61
Shobhan Bhatt <shobhanb@gmail.com>
2025-07-29 10:29:25 +0530

    Bugfix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // production: true,
-  // apiBaseUrl: 'https://cfmf.cfgames.site/api',
+  apiBaseUrl: 'https://cfmf.cfgames.site/api',
   // affiliateId: 31316,
   // affiliateName: 'Crossfit Monkey Flag',
   // year: 2025,
